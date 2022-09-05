ccdb8367ecce65ba41fa5dc5087c3170264d8ea7
Ikaro Chagas <ikarochagas.ig@gmail.com>
2022-09-05 11:27:18 -0300

    Update index.tsx

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,14 @@
 import * as C from './styles'
 
 type Props = {
-    type: string
-    placeholder: string
-    value: string | number
-    onChange: (e: any) => void
+    type?: string
+    placeholder?: string
+    value?: string | number
+    onChange?: (e: any) => void
 }
 
 export const Input = ({ type, placeholder, value, onChange }: Props) => {
+
     return (
         <C.Input type={type} placeholder={placeholder} value={value} onChange={onChange} />
     );
