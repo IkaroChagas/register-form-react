099ca347675fcfe78a73d809379498b0d0f98fb7
Ikaro Chagas <ikarochagas.ig@gmail.com>
2022-09-05 10:44:11 -0300

    Update index.tsx

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Context } from "../../contexts/context";
